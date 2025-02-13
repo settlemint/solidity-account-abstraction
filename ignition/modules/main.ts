b21f6c9f81f31e8cac36fd919c351e7cf0ba1bbe
Daan Poron <daan@poron.be>
2025-02-13 16:25:04 +0100

    cleanup the AA dependencies + ignition module

--- ignition/modules/main.ts
+++ ignition/modules/main.ts
@@ -2,10 +2,10 @@ import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
 
 export default buildModule("AccountAbstraction", (m) => {
 	// Deploy EntryPoint first since accounts need it
-	const entryPoint = m.contract("SmartAccountEntryPoint", []);
+	const entryPoint = m.contract("SmartEntryPoint", []);
 
-	// Deploy factory that will use this EntryPoint
-	const smartAccountFactory = m.contract("SmartAccountFactory", []);
+	// Deploy factory with EntryPoint address as constructor parameter
+	const smartAccountFactory = m.contract("SmartAccountFactory", [entryPoint]);
 
 	return { entryPoint, smartAccountFactory };
 });
