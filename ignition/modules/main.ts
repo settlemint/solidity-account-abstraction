3701cc87697cbebd3d7cbfa0cb71a4e5fa900079
Daan Poron <daan@poron.be>
2025-03-31 11:17:43 +0200

    fix: also install Paymaster in ignition module

--- ignition/modules/main.ts
+++ ignition/modules/main.ts
@@ -1,11 +1,9 @@
 import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
 
 export default buildModule("AccountAbstraction", (m) => {
-	// Deploy EntryPoint first since accounts need it
 	const entryPoint = m.contract("SmartEntryPoint", []);
-
-	// Deploy factory with EntryPoint address as constructor parameter
 	const smartAccountFactory = m.contract("SmartAccountFactory", [entryPoint]);
+	const paymaster = m.contract("UnrestrictedPaymaster", [entryPoint]);
 
-	return { entryPoint, smartAccountFactory };
+	return { entryPoint, smartAccountFactory, paymaster };
 });
